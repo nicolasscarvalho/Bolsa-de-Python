d38591cec6d126169a186661dc6df44483dca483
Nícolas Carvalho <nicolassprofissional@gmail.com>
2023-05-16 19:58:58 +0000

    adicionando POO no servidor

--- Introducao/revisao-de-POO/servidor/main.py
+++ Introducao/revisao-de-POO/servidor/main.py
@@ -1,35 +1,53 @@
 import socket
 import os
 
-servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Servidor() :
 
+    def __init__(self, ip:str = 'localhost', porta:int = 4000):
 
-
-servidor.bind(('localhost', 4000))
-servidor.listen(1)
-
-cliente_conexao, cliente_endereco = servidor.accept()
-
-nome_arquivo = f'./arquivos/{ cliente_conexao.recv(1024).decode() }'
-
-print(cliente_conexao, cliente_endereco, nome_arquivo)
-
-
-if os.path.isfile(nome_arquivo):
-
-    if os.stat(nome_arquivo) >= 31.25:
+        self.ip = ip
+        self.porta = porta
+        self.servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
-        with open(nome_arquivo, 'wb') as arquivo:
 
-            for dados in arquivo.readlines():
-                cliente_conexao.send(dados)
-            
-            print('Arquivo enviado para o cliente')
 
-    else:
-        print('Arquivo menor que o esperado')
-        cliente_conexao.send('Arquivo menor que o esperado'.encode())
+    def escutar(self, qt:int = 1):
+        self.servidor.bind(('localhost', 4000))
+        self.servidor.listen(qt)
 
-else:
-    print('Arquivo não encontrado no servidor')
-    cliente_conexao.send('Arquivo nao encontrado no servidor'.encode())
+
+
+    def aceitar_conexao(self):
+        return self.servidor.accept()
+
+
+
+    def receber_arquivo(self):
+        cliente_conexao, cliente_endereco = self.aceitar_conexao()
+        nome_arquivo = cliente_conexao.recv(1024)
+
+
+        with open(f'./arquivos/{nome_arquivo.decode()}', 'wb') as arquivo:
+
+            while True:
+                dado_recebido = cliente_conexao.recv(10**6)
+
+                print(dado_recebido)
+
+                if dado_recebido:
+                    arquivo.write(dado_recebido)
+                else:
+                    break
+
+
+
+    def finalizar_conexao(self):
+        self.servidor.close()
+
+
+servidor: Servidor = Servidor()
+
+servidor.escutar()
+servidor.aceitar_conexao()
+servidor.receber_arquivo()
+servidor.finalizar_conexao()
